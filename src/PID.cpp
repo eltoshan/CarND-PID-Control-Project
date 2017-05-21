20cbb8a89121d2a85a87766f9d8ab7d69c1a495c
eltoshan <eltoshan@gmail.com>
2017-05-21 12:34:55 -0400

    initial implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,23 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
+    return;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+    return;
 }
 
 double PID::TotalError() {
+    return -Kp*p_error - Kd*d_error - Ki*i_error;
 }
 
